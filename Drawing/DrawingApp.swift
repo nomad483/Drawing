af91c840f5a4978d892ccad01d4c5aa85877915d
nomad483 <nicolaizakluka@gmail.com>
2023-05-04 01:04:58 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mykola Zakluka on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
